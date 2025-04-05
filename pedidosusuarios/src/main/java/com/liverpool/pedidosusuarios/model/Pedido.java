b67e68b46a6044862a7f408d70733355e50fd557
Ariadna <arixgos@gmail.com>
2025-04-04 19:14:42 -0600

    Creacion de entidades

---
+++ pedidosusuarios/src/main/java/com/liverpool/pedidosusuarios/model/Pedido.java
@@ -0,0 +1,70 @@
+package com.liverpool.pedidosusuarios.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "pedidos")
+public class Pedido
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idPedido;
+    private String codigo_pedido;
+    private Integer cantidad;
+    private Double precio;
+    @ManyToOne
+    @JoinColumn(name = "idUsuario")
+    private Usuario usuario;
+
+    public Pedido() {
+
+    }
+
+    public Pedido(Integer idPedido, String codigo_pedido, Integer cantidad, Double precio, Usuario usuario) {
+        this.idPedido = idPedido;
+        this.codigo_pedido = codigo_pedido;
+        this.cantidad = cantidad;
+        this.precio = precio;
+        this.usuario = usuario;
+    }
+
+    public Integer getIdPedido() {
+        return idPedido;
+    }
+
+    public void setIdPedido(Integer idPedido) {
+        this.idPedido = idPedido;
+    }
+
+    public String getCodigo_pedido() {
+        return codigo_pedido;
+    }
+
+    public void setCodigo_pedido(String codigo_pedido) {
+        this.codigo_pedido = codigo_pedido;
+    }
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+}
--- pedidosusuarios/src/main/java/com/liverpool/pedidosusuarios/model/Usuario.java
+++ pedidosusuarios/src/main/java/com/liverpool/pedidosusuarios/model/Usuario.java
@@ -1,4 +1,93 @@
 package com.liverpool.pedidosusuarios.model;
 
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "usuarios")
 public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer  idUsuario;
+    private String nombre;
+    private String apellidoP;
+    private String apellidoM;
+    @Column(unique = true)
+    private String correo;
+    private String direccion;
+
+    @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL)
+    private List<Pedido> pedidos;
+
+    public Usuario() {
+
+    }
+
+    public Usuario(Integer idUsuario, String nombre, String apellidoP, String apellidoM, String correo, String direccion, List<Pedido> pedidos) {
+        this.idUsuario = idUsuario;
+        this.nombre = nombre;
+        this.apellidoP = apellidoP;
+        this.apellidoM = apellidoM;
+        this.correo = correo;
+        this.direccion = direccion;
+        this.pedidos = pedidos;
+    }
+
+    public Integer getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(Integer idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidoP() {
+        return apellidoP;
+    }
+
+    public void setApellidoP(String apellidoP) {
+        this.apellidoP = apellidoP;
+    }
+
+    public String getApellidoM() {
+        return apellidoM;
+    }
+
+    public void setApellidoM(String apellidoM) {
+        this.apellidoM = apellidoM;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public List<Pedido> getPedidos() {
+        return pedidos;
+    }
+
+    public void setPedidos(List<Pedido> pedidos) {
+        this.pedidos = pedidos;
+    }
 }
