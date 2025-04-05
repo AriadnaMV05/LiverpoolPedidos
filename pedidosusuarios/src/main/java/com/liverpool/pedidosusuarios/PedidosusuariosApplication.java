70a3769ab0fca1a5ea39444d764bcbd13bde45a5
Ariadna <arixgos@gmail.com>
2025-04-04 18:48:26 -0600

    Inicio de proyecto

---
+++ pedidosusuarios/src/main/java/com/liverpool/pedidosusuarios/PedidosusuariosApplication.java
@@ -0,0 +1,13 @@
+package com.liverpool.pedidosusuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidosusuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedidosusuariosApplication.class, args);
+	}
+
+}
---
+++ pedidosusuarios/src/test/java/com/liverpool/pedidosusuarios/PedidosusuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liverpool.pedidosusuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidosusuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
